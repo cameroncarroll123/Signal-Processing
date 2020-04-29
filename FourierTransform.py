6069602abccfc7fb45c6f2e85dc0d99d8cd5760c
cameroncarroll123 <cameron.carroll123@gmail.com>
2020-04-29 17:53:15 -0500

    Working FourierTransform

--- FourierTransform.py
+++ FourierTransform.py
@@ -9,21 +9,31 @@ from math import pi
 import numpy as np
 import matplotlib.pyplot as plt
 
+fs = 200 #Hz sampling rate
+t0 = 1/fs #sec sample period
+T = 3 #Sec
+n = np.arange(0,T,t0) #Samples
+N = n.size
+f0 = 1/T
+fz = np.arange(-fs/2,fs/2,f0)
 
-f = 100
-T = 1 / f
-timeStep = .0001
-t = np.arange(0,3*T,timeStep)
+
+f = 50
 w = 2 * pi * f
-
-sig = np.cos(w * t)
+sig = np.sin(w * n)
 
 plt.figure()
-plt.plot(t,sig)
+plt.plot(n,sig)
 
-SIG = np.fft.fftshift(np.fft.fft(sig))
-# SIG = np.fft.fft(sig)
-fz = t/(t.size*timeStep)
-# fz = np.fft.fftfreq(t.size,.01)
+# SIGf = np.fft.fftshift(np.fft.fft(sig))/N
+SIGf = np.fft.fft(sig)/N
+SIGf[np.abs(SIGf) < .000000001] = 0
+sig = np.fft.ifft(SIGf*N)
+# fz = np.arange(-F/2,F/2,f0)
+fz = np.fft.fftfreq(N,t0)
 plt.figure()
-plt.plot(fz,SIG)
+plt.stem(fz,np.abs(SIGf),use_line_collection = True)
+plt.figure()
+plt.stem(fz,np.angle(SIGf),use_line_collection = True)
+plt.figure()
+plt.plot(n,np.real(sig))
