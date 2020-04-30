885627a77ff23ffb2f55afc904c094c66bccf9ad
cameroncarroll123 <cameron.carroll123@gmail.com>
2020-04-30 16:26:26 -0500

    Organized FourierTransform

--- FourierTransform.py
+++ FourierTransform.py
@@ -9,31 +9,48 @@ from math import pi
 import numpy as np
 import matplotlib.pyplot as plt
 
-fs = 200 #Hz sampling rate
-t0 = 1/fs #sec sample period
-T = 3 #Sec
-n = np.arange(0,T,t0) #Samples
-N = n.size
-f0 = 1/T
-fz = np.arange(-fs/2,fs/2,f0)
+# Sampling rate
+fs = 2000 #Hz 
 
+# Sample period
+t0 = 1/fs #sec 
 
-f = 50
-w = 2 * pi * f
+# Length of signal in time domain (3 secounds)
+T = 3 #sec
+
+# Samples n = 0 - T - 1 spaced by sample period
+n = np.arange(0,T,t0) 
+
+# Size of signal (number of samples)
+N = n.size 
+
+# Spacing in the frequency domain
+f0 = 1/T 
+
+# Signal creation
+sigFreq = 250
+w = 2 * pi * sigFreq
 sig = np.sin(w * n)
 
 plt.figure()
 plt.plot(n,sig)
 
-# SIGf = np.fft.fftshift(np.fft.fft(sig))/N
-SIGf = np.fft.fft(sig)/N
-SIGf[np.abs(SIGf) < .000000001] = 0
-sig = np.fft.ifft(SIGf*N)
-# fz = np.arange(-F/2,F/2,f0)
-fz = np.fft.fftfreq(N,t0)
+# Implementaion with calculating f myself
+SIGf = np.fft.fftshift(np.fft.fft(sig))/N
+f = np.arange(-fs/2,fs/2,f0)
+sig2 = np.fft.ifft(np.fft.fftshift(SIGf*N))
+
+# Implementation with fftfreq
+# SIGf = np.fft.fft(sig)/N
+# f = np.fft.fftfreq(N,t0)
+# sig2 = np.fft.ifft(SIGf*N)
+
+# Correct quantization errors that will show up in phase
+SIGf[np.abs(SIGf) < .0000000001] = 0
+
 plt.figure()
-plt.stem(fz,np.abs(SIGf),use_line_collection = True)
+plt.stem(f,np.abs(SIGf),use_line_collection = True)
 plt.figure()
-plt.stem(fz,np.angle(SIGf),use_line_collection = True)
+plt.stem(f,np.angle(SIGf),use_line_collection = True)
 plt.figure()
-plt.plot(n,np.real(sig))
+plt.plot(n,np.real(sig2))
