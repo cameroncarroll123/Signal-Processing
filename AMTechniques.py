d49248ab74ccde16c131bfe315ab56b42e1c1153
cameroncarroll123 <cameron.carroll123@gmail.com>
2020-05-04 19:27:47 -0500

    Improved Signals and change AMTechs

--- AMTechniques.py
+++ AMTechniques.py
@@ -9,22 +9,35 @@ from scipy.io import wavfile
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
+from math import pi
+import Signals as sigs
 
 
 path = os.getcwd() + "\questions.wav"
 fs, m = wavfile.read(path)
 # sd.play(m,fs)
-t0 = 1/fs #sec 
-N = m.size
-T = N * t0
-n = np.arange(0,T,t0)
-f0 = 1 / T
+# t0 = 1/fs #sec 
+# N = m.size
+# T = N * t0
+# n = np.arange(0,T,t0)
+# f0 = 1 / T
+sig = sigs(m, fs)
+n = np.range(0,sig.waveDuration,sig.sampT)
 
 plt.figure()
 plt.plot(n,m)
 
-M = np.fft.fft(m)/N
-f = np.fft.fftfreq(N,t0)
+# M = np.fft.fft(m)/N
+# f = np.fft.fftfreq(N,t0)
 
-plt.figure()
-plt.plot(f,M)
+# plt.figure()
+# plt.plot(f,M)
+
+# carrierFreq = 250
+# w = 2 * pi * carrierFreq
+# carrier = np.cos(w * n)
+
+# amSig = m * carrier
+
+# plt.figure()
+# plt.plot(n,m)
--- Signals.py
+++ Signals.py
@@ -4,8 +4,27 @@ Created on Mon May  4 18:20:45 2020
 
 @author: Cameron
 """
+import os
+from scipy.io import wavfile
+import sounddevice as sd
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi
 
 
 class Signals:
-  def __init__(self, data, fs):
-    pass
+  def __init__(self, data, fs, blockSize = None):
+    self.sampT = 1 / fs
+    self.dataN = data.size
+    self.waveDuration = self.sampT * self.dataN
+    blockSize = self.dataN if blockSize is None else blockSize
+    self.fftRes = fs / blockSize
+    self.idx = np.arange(0, self.waveDuration,self.sampT)
+    
+path = os.getcwd() + "\questions.wav"
+fs, m = wavfile.read(path)
+
+sig = Signals(m, fs)
+
+plt.figure()
+plt.plot(sig.idx,m)
